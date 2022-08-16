3b90f9849080dca0de43e90af9c380d4b6ef3546
waterburner <Waterburner@users.noreply.github.com>
2022-08-15 21:13:46 -0500

    added GuestApproach component

--- src/components/GuestApproach.js
+++ src/components/GuestApproach.js
@@ -1,17 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 
 export default function GuestApproach() {
     return (
-        <div>
+        <div className="guest-approach">
             <h1>Guest approach guide</h1>
 
-            <div className="greeting_section">
-                <h3 className="greeting-header">Greeting</h3>
-                <p className="greeting-description">
+            <div className="order-section">
+                <h3 className="section-header">Greeting</h3>
+                <p className="section-description">
                     Start the conversation with greeting. Something like "good
-                    evening" or "hello". <i>Optionaly</i> ask how they're doing.
+                    evening" or "hello".{" "}
+                    <i>Optionaly: ask how they're doing.</i>
                     Contiue with offering a drink, something like:
-                    <u>
+                    <ul>
                         <li>
                             "would you like to start with a cocktail, glass of
                             wine or a beer?"
@@ -25,29 +26,47 @@ export default function GuestApproach() {
                             would you be interested in a …(barkeeper special)?
                         </li>
                         <li>etc.</li>
-                    </u>
-                    <br />
+                    </ul>
+                    <br /> <br />
+                    <span>
+                        If guest is asking about{" "}
+                        <b className="option"> beers</b>: say that we have
+                        around 30 craft beers on draft and that you'll bring the
+                        list. Althought if guest knows
+                        <b className="option"> exactly what they want</b> check
+                        in the toast if we have it at the moment.
+                    </span>
+                    <span>
+                        IMPORTANT to know all the bottle beers that we have
+                        (it's basic ~6).
+                    </span>
+                    <br /> <br />
+                    <i className="order-putting-manual">
+                        LINK TO PUTTING DRINKS ORDER WILL BE HERE
+                    </i>
+                    <br /> <br />
                     After taking any and all orders always make sure to use
                     "affirmation therm" ("awesome", "great", "you got it") — so
                     the guest would know that you heard him, and also make sure
                     you always double check the order ("old fasion, nice!",
                     "awesome, Casa Migos Margarita!"). Then you may ask if they
                     have been dining with us before (or have been here before).
-                    <br />
-                    <p className="description-options">
-                        <div className="description-option-choice">
-                            If <b className="option">yes</b> — ask if they would
-                            like to start with an Appetaizer, a soup or a salad?
-                            <br />
+                    <br /> <br />
+                    <p className="options">
+                        <div className="option-choice">
+                            If <b className="option"> yes</b> — ask if they
+                            would like to start with an Appetaizer, a soup or a
+                            salad?
+                            <br /> <br />
                         </div>
-                        <div className="description-option-choice">
-                            If <b className="option">no</b> - recommend your
+                        <div className="option-choice">
+                            If <b className="option"> no</b> - recommend your
                             favorite Appetaizer and inform which one is most
                             popular (limit to 2-3 recommendation). I usually
                             finish it with "we don't really have bad choices,
                             but this is my favorite and other one is most
                             popular".
-                            <br />
+                            <br /> <br />
                             Also tell them about your favorite entree, the most
                             popular one, maybe a good salad if they're looking
                             for something lighter. Inform that most of the
@@ -60,63 +79,139 @@ export default function GuestApproach() {
                     If guest(s) seems like they're not sure what to order
                     conclude the conversation with something like "I'll be back
                     with … (name the ordered drinks) and you'll have couple
-                    minutes with the menus" <br />
+                    minutes with the menus" <br /> <br />
                     or if they do order appetaizer — say same thing and name the
                     appetaizer (you're checking if you heard it right) And to
                     finalize the approach say something like "If you need
                     anything my name is…", or just "My name is… I'll be taking
                     care of you today".
+                    <i className="order-putting-manual">
+                        LINK TO PUTTING Appetaizer ORDER WILL BE HERE
+                    </i>
                 </p>
             </div>
 
-            <div className="meal-order-section">
-                <h3 className="meal-order-header">Taking the entree order</h3>
+            <div className="order-section">
+                <h3 className="section-header">Taking the entree order</h3>
 
-                <p className="meal-order-description">
+                <p className="section-description">
                     If the drinks are ready as soon as you left the table — just
                     deliver them and ask if there are any questions. If no —
                     just say I'll give you a moment.
-                    <br />
-                    Before appetaizers ready (give it like 5 minutes), or if you
-                    see that guests are ready — approach them and ask if they
-                    have any questions or they're ready to order.
-                    <br />
+                    <br /> <br />
+                    Before appetaizers are ready (give it like 5 minutes), or if
+                    you see that guests are ready — approach them and ask if
+                    they have any questions or they're ready to order.
+                    <br /> <br />
                     If the guest ordering
-                    <b className="option">a salad as meal </b> ask would they
+                    <b className="option"> a salad as meal </b> ask would they
                     like to add any chincken, shrimp or salmon on that salad.
                     You can always ask to add shrimp to salad that has chicken
                     already (whatever sounds good, basicaly). Also ask if they
-                    would like to start with a soup. <br />
-                    If it's <b className="option">a steak</b> — how they would
+                    would like to start with a soup. <br /> <br />
+                    If it's <b className="option"> a steak</b> — how they would
                     like the steak to be cooked and also confirm when they say
                     temperature how it's going to be (med rare — worm red
                     center, etc). After that you can ask if they would like to
-                    add shrimp to the steak. <br />
+                    add shrimp to the steak. <br /> <br />
                     If guest is ordering
-                    <b className="option">salmon or pork chop</b> — mention that
-                    the default temperature is medium well (some ppl like salmon
-                    less or more cooked. Also some people are scared of
+                    <b className="option"> salmon or pork chop</b> — mention
+                    that the default temperature is medium well (some ppl like
+                    salmon less or more cooked. Also some people are scared of
                     undercooked pork).
-                    <br />
+                    <br /> <br />
                     If the guest choosing
-                    <b className="option">a soup as meal</b> — ask if they want
-                    to add a salad as well to the order. <br />
-                    In case of if order is <b className="option">a burger</b> -
+                    <b className="option"> a soup as meal</b> — ask if they want
+                    to add a salad as well to the order. <br /> <br />
+                    In case of if order is <b className="option"> a burger</b> -
                     ask how the burger should be cooked and if everything on
                     burger sounds good (lots of "no tomato", "no onions").
-                    <br />
+                    <br /> <br />
                     If people want
                     <b className="option">
+                        {" "}
                         "side of soup or salad, or substitude side for soup or
-                        salad"
+                        salad"{" "}
                     </b>
                     — inform them that soup or salads sold saperatly and/or that
-                    we don't have "side salad". <br />
+                    we don't have "side salad". <br /> <br />
                     After everyone is ordered double check with them ("let me
                     /double/ check if I have everything right"). Make sure
                     you're naming all of the modifications and sides to minimize
-                    possibility of mistakes. <br />
+                    possibility of mistakes. <br /> <br />
+                    If guest orders
+                    <b className="option">
+                        {" "}
+                        appetaizer and entree at same time
+                    </b>{" "}
+                    - take orders but send it separatly (entree 5 minutes after
+                    appetaizer)
+                    <i className="order-putting-manual">
+                        LINK TO PUTTING ENTREE/appetaizer ORDER WILL BE HERE
+                    </i>
                 </p>
+
+                <div className="order-section">
+                    <h3 className="section-header">After taking orders</h3>
+                    <p className="section-description">
+                        If you see <b className="option"> an alcohol drink </b>
+                        bellow half offer another one (in ex. "Would you like
+                        another cotail/beer/old fasion/cab/wine?" etc). If it's
+                        <b className="option"> NA reffilible drink</b> — just
+                        refill it without asking. Any
+                        <b className="option">
+                            {" "}
+                            appetaizer/soup/salad plates that are empty/finished{" "}
+                        </b>
+                        must be taken away to clean the table and make space for
+                        entrees. Prior to
+                        <b className="option"> steak/pork chop arrival</b> try
+                        your best to bring steak knife (make sure the knife is
+                        clean and polished).
+                        <br /> <br />
+                        <b className="option"> After serving the entrees </b>
+                        check with guest if everything looks good and/or to
+                        their liking. Ask if you could provide anything else at
+                        the moment (maybe another drink?)
+                        <br /> <br />
+                        <b className="option"> If steaks are delivered</b> —
+                        good practice is to ask guest to cut the steak to make
+                        sure the guest is satisfied with temperature it's
+                        cooked.
+                        <br /> <br />
+                        <b className="option">
+                            In 2-5 minutes come back to check on guests with
+                            meal
+                        </b>
+                        : ask if everything taste good/delicius.
+                        <br /> <br />
+                        As soon as you see
+                        <b className="option">
+                            empty plates (or meal finished)
+                        </b>
+                        check with guest if you may take the plate or if they
+                        need to go boxes.
+                        <br /> <br />
+                    </p>
+                </div>
+
+                <div className="order-section">
+                    <h3 className="section-header">Dessert ordering</h3>
+                    <p className="section-description">
+                        After prebussing the table (or if guest asks for check)
+                        ask if they would like any desert or chocolate
+                        martini/Irish coffee to finish the meal.
+                        <br /> <br />
+                        <b className="option"> If yes</b> that our desserts
+                        are… name desserts.
+                        <br /> <br />
+                        <b className="option"> If no</b> — bring the check (or
+                        show it on your toast).
+                        <i className="order-putting-manual">
+                            LINK TO PUTTING dessert ORDER WILL BE HERE
+                        </i>
+                    </p>
+                </div>
             </div>
         </div>
     );
--- src/components/app.js
+++ src/components/app.js
@@ -8,6 +8,7 @@ import Homepage from "./homepage/Homepage";
 import Menus from "./menu/Menus.js";
 import AllMenuItems from "./menu/AllMenuItems";
 import ChoosenMenu from "./menu/ChoosenMenu";
+import GuestApproach from "./GuestApproach";
 import Credits from "./credits";
 import Sidework from "./sidework";
 import NoMatch from "./NoMatch";
@@ -22,29 +23,27 @@ export default class App extends Component {
 
                         <Routes>
                             <Route exact path="/" element={<Homepage />} />
-
                             <Route path="/menus" element={<Menus />} />
-
                             <Route
                                 path="/all_menu_items"
                                 element={<AllMenuItems />}
                             />
-
                             <Route
                                 path="/allmenuitems"
                                 element={<AllMenuItems />}
                             />
-
                             <Route
                                 exact
                                 path="/menu/:slug"
                                 element={<ChoosenMenu />}
                             />
 
+                            <Route
+                                path="/guest-approach"
+                                element={<GuestApproach />}
+                            />
                             <Route path="/credits" element={<Credits />} />
-
                             <Route path="/sideworks" element={<Sidework />} />
-
                             <Route path="*" element={<NoMatch />} />
                         </Routes>
 
