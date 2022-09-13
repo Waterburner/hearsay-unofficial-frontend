44a0743becf9489327bfc10970f5b9df82ebc93b
waterburner <Waterburner@users.noreply.github.com>
2022-09-13 18:46:40 -0500

    created  folder guides and added content for SpecialRequests.js component in it

---
+++ src/components/SpecialRequests.js
@@ -0,0 +1,73 @@
+import React from "react";
+
+export default function SpecialRequests() {
+    return (
+        <div className="special-requests guide">
+            <h1>SPECIAL REQUESTS</h1>
+            <p className="header-details">
+                As in any restaurant there is always special orders for various
+                reasons. Sometimes it's an allergies, other times it's "I don't
+                like"/"I don't eat". Ocasinally it's something that easy to do
+                or fix. Other times it's like a new dish is being made. <br />
+                This guide will cover most common requests and how to deal with
+                it and how put it in the system (hopefully).
+            </p>
+            <h3>Allergies</h3>
+            <p className="header-details">
+                Also "intolerances" (wheat, dairy, etc). Important stuff.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Aperiam, laboriosam nemo? Harum, laudantium enim natus, animi
+                dolorum quos, voluptates repellendus labore nemo nobis quas
+                possimus consectetur? Quasi cum ipsum delectus ut earum?
+            </p>
+            <ul>
+                <li>Do this #1</li>
+                <li>Do this #2</li>
+                <li>Do this #3</li>
+            </ul>
+            <ul>
+                <li>put it in the system like that 1</li>
+                <li>put it in the system like that 2</li>
+                <li>put it in the system like that 3</li>
+                <li>put it in the system like that 4</li>
+            </ul>
+            <h3>Picky people and "I don't like that"</h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet,
+                velit esse deserunt, culpa quos tenetur suscipit magnam earum
+                praesentium unde vitae, modi impedit neque.
+            </p>
+            <ul>
+                <li>Do this #1</li>
+                <li>Do this #2</li>
+                <li>Do this #3</li>
+            </ul>
+            <ul>
+                <li>put it in the system like that 1</li>
+                <li>put it in the system like that 2</li>
+                <li>put it in the system like that 3</li>
+                <li>put it in the system like that 4</li>
+            </ul>
+            <h3>Upselling, Upcharging, adding stuff</h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Sapiente aperiam voluptatum eaque possimus odio inventore
+                tenetur itaque accusantium veritatis. Nemo commodi pariatur sint
+                veritatis voluptatibus ex ad. Fugiat cum tempora tempore qui.
+            </p>
+            <ul>
+                <li>Do this #1</li>
+                <li>Do this #2</li>
+                <li>Do this #3</li>
+            </ul>
+            <ul>
+                <li>put it in the system like that 1</li>
+                <li>put it in the system like that 2</li>
+                <li>put it in the system like that 3</li>
+                <li>put it in the system like that 4</li>
+            </ul>
+        </div>
+    );
+}
