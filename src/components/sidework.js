cf8e224758755ac34dea484ab33d2e31eae58665
waterburner <Waterburner@users.noreply.github.com>
2022-09-13 18:32:44 -0500

    added teamplate for sidework component

--- src/components/sidework.js
+++ src/components/sidework.js
@@ -4,6 +4,7 @@ export default function Sidework() {
     return (
         <div className="sidework">
             <h1>SERVERS</h1>
+            <h3>Opening sidework</h3>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
                 voluptas harum, voluptatibus nihil at blanditiis qui voluptates
@@ -18,7 +19,20 @@ export default function Sidework() {
                 </ul>
             </p>
 
-            <h1>HOSTESS</h1>
+            <h3>For AM shift leaving</h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+                voluptas harum, voluptatibus nihil at blanditiis qui voluptates
+                magni nemo perspiciatis quibusdam, recusandae tempore sapiente
+                minima saepe molestias sint! Fuga veniam labore corrupti.
+                <ul>
+                    <li>todo #1</li>
+                    <li>todo #2</li>
+                    <li>todo #3</li>
+                </ul>
+            </p>
+
+            <h3>Durring the shift</h3>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
                 voluptas harum, voluptatibus nihil at blanditiis qui voluptates
@@ -30,6 +44,83 @@ export default function Sidework() {
                     <li>todo #3</li>
                     <li>todo #4</li>
                     <li>todo #5</li>
+                </ul>
+            </p>
+            <h3>Closing sidework</h3>
+
+            <span>Sidework for staff who's leaving early</span>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+                voluptas harum, voluptatibus nihil at blanditiis qui voluptates
+                magni nemo perspiciatis quibusdam, recusandae tempore sapiente
+                minima saepe molestias sint! Fuga veniam labore corrupti.
+                <ul>
+                    <li>todo #1</li>
+                    <li>todo #2</li>
+                    <li>todo #3</li>
+                    <li>todo #4</li>
+                    <li>todo #5</li>
+                </ul>
+            </p>
+            <span>Closing staff</span>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+                voluptas harum, voluptatibus nihil at blanditiis qui voluptates
+                magni nemo perspiciatis quibusdam, recusandae tempore sapiente
+                minima saepe molestias sint! Fuga veniam labore corrupti.
+                <ul>
+                    <li>todo #1</li>
+                    <li>todo #2</li>
+                    <li>todo #3</li>
+                </ul>
+            </p>
+
+            <h1>HOSTESS</h1>
+            <h3>Opening</h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+                voluptas harum, voluptatibus nihil at blanditiis qui voluptates
+                magni nemo perspiciatis quibusdam, recusandae tempore sapiente
+                minima saepe molestias sint! Fuga veniam labore corrupti.
+                <ul>
+                    <li>todo #1</li>
+                    <li>todo #2</li>
+                    <li>todo #3</li>
+                    <li>todo #4</li>
+                    <li>todo #5</li>
+                </ul>
+            </p>
+            <h3>Durring shift</h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+                voluptas harum, voluptatibus nihil at blanditiis qui voluptates
+                magni nemo perspiciatis quibusdam, recusandae tempore sapiente
+                minima saepe molestias sint! Fuga veniam labore corrupti.
+                <ul>
+                    <li>todo #1</li>
+                    <li>todo #2</li>
+                    <li>todo #3</li>
+                    <li>todo #4</li>
+                    <li>todo #5</li>
+                </ul>
+            </p>
+            <h3>Closing shift</h3>
+            <span>Leaving early</span>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+                voluptas harum, voluptatibus nihil at blanditiis qui voluptates
+                <ul>
+                    <li>todo #1</li>
+                    <li>todo #2</li>
+                </ul>
+            </p>
+            <span>Closing the restaurant</span>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+                voluptas harum, voluptatibus nihil at blanditiis qui voluptates
+                <ul>
+                    <li>todo #1</li>
+                    <li>todo #2</li>
                 </ul>
             </p>
         </div>
